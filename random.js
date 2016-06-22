e8e6b1650e35a2ea338ebb9a7fc68b8b576b5378
Chakri Reddy <chkryreddy@gmail.com>
2016-06-22 14:21:54 +0530

    Updated UI & Added Previous Option to check last image

--- random.js
+++ random.js
@@ -1,11 +1,11 @@
 document.getElementById("yesButton").addEventListener("click", chkry);
 
 
+var ArrayStore = [];
+
+var images = ["http://kindersay.com/files/images/goat.png", "http://cdn1-www.dogtime.com/assets/uploads/2011/01/file_23262_entlebucher-mountain-dog-460x290.jpg", "http://animal-dream.com/data_images/cat/cat3.jpg", "http://images.all-free-download.com/images/graphiclarge/cartoon_cow_clip_art_6772.jpg"];
 
 function chkry() {
-
-    var images = ["http://kindersay.com/files/images/goat.png", "http://cdn1-www.dogtime.com/assets/uploads/2011/01/file_23262_entlebucher-mountain-dog-460x290.jpg", "http://animal-dream.com/data_images/cat/cat3.jpg", "http://images.all-free-download.com/images/graphiclarge/cartoon_cow_clip_art_6772.jpg"]
-
 
     var presentImage = document.getElementById("sourceImage").src;
     console.log(presentImage);
@@ -13,6 +13,9 @@ function chkry() {
     var index = images.indexOf(presentImage);
     console.log(index);
 
+    var prevIndex = images.indexOf(presentImage);
+
+    var storePrevIndex = PushArray(prevIndex);
 
     if (index == -1) {
 
@@ -49,3 +52,31 @@ function calculate() {
     return random;
 
 }
+
+
+// Storing Values in array
+
+function PushArray(prevIndex){
+
+    ArrayStore.push(prevIndex)
+    console.log(ArrayStore);
+}
+
+
+
+
+
+//Storing values in array end
+
+document.getElementById("noButton").addEventListener("click", NoClicked);
+
+function NoClicked(){
+
+    var prevIndex = ArrayStore[ArrayStore.length-1];
+    var imagesrc = images[prevIndex];
+
+    document.getElementById("sourceImage").src = imagesrc;
+
+
+}
+
