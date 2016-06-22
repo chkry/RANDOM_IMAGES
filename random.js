6a7da3c6f0490d4324fc948253a21c61129e9177
Chakri Reddy <chkryreddy@gmail.com>
2016-06-22 11:10:32 +0530

    Fixed Same image reload using do while loop

--- random.js
+++ random.js
@@ -1,38 +1,52 @@
-
-
 document.getElementById("yesButton").addEventListener("click", chkry);
 
 
 
-function chkry(){
+function chkry() {
 
-            var images = ["http://kindersay.com/files/images/goat.png","http://cdn1-www.dogtime.com/assets/uploads/2011/01/file_23262_entlebucher-mountain-dog-460x290.jpg","http://animal-dream.com/data_images/cat/cat3.jpg","http://images.all-free-download.com/images/graphiclarge/cartoon_cow_clip_art_6772.jpg"]
+    var images = ["http://kindersay.com/files/images/goat.png", "http://cdn1-www.dogtime.com/assets/uploads/2011/01/file_23262_entlebucher-mountain-dog-460x290.jpg", "http://animal-dream.com/data_images/cat/cat3.jpg", "http://images.all-free-download.com/images/graphiclarge/cartoon_cow_clip_art_6772.jpg"]
 
 
-            if(randomValue == null){
-                randomValue = 0;
-            }
+    var presentImage = document.getElementById("sourceImage").src;
+    console.log(presentImage);
 
-            var randomValue = calculate(randomValue);
+    var index = images.indexOf(presentImage);
+    console.log(index);
 
-            var imagesrc = images[randomValue];
 
-            document.getElementById("sourceImage").src=imagesrc;
-            document.getElementById("sourceImage").alt=randomValue;
+    if (index == -1) {
+
+        console.log("index in if condition"+index);
+        var randomValue = calculate();
+        console.log("RandomValue in if condition"+randomValue);
+        var imagesrc = images[randomValue];
+        console.log(imagesrc);
+        document.getElementById("sourceImage").src = imagesrc;
+
+
+    } else {
+
+
+        console.log("index in else condition"+index);
+
+        do{
+            var randomValue = calculate();
+        }while(index == randomValue)
+        console.log("RandomValue in else condition"+randomValue);
+
+        var imagesrc = images[randomValue];
+        document.getElementById("sourceImage").src = imagesrc;
+    }
+
+
 
 
 }
 
 
-function calculate(oldValue){
-
-    var random = Math.random()*3;
-    var random = random.toFixed(0);
-    console.log("The Random value is :"+random);
-    console.log("The oldValue is "+oldValue);
-    if(oldValue !== random){
-       return random
-    }
-    return -1;
+function calculate() {
+        var random = Math.random() * 3;
+        var random = random.toFixed(0);
+    return random;
 
 }
